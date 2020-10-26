610b18f5658bc73bdb66531a1c3230e9ee07357e
alejandra-medina <alejandramedina1215@gmail.com>
2020-10-26 19:16:03 -0300

    avances

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import validator from './validator.js';
+import {validator} from './validator.js';
 
 console.log(validator);
+
+document.getElementById("validar").onclick= function(){
+ 
+  let nombre = document.getElementById("nombre").value;
+  let creditCardNumber = document.getElementById("numerotarjetaa").value;
+  let tarjetaEnmascarada = creditCardNumber;
+    document.getElementById("salir").style.display="block";
+    document.getElementById("validar").style.display= "none";
+
+  console.log (nombre);
+  console.log (creditCardNumber);
+  //console.log (tarjetaEnmascarada);
+  
+  document.getElementById("numeroescondido").innerHTML = validator.isValid(creditCardNumber)
+  document.getElementById("sobrescribir").innerHTML = " Hola " + nombre + " Tu Tarjeta " + validator.maskify(tarjetaEnmascarada) + " es " + validator.isValid(creditCardNumber);
+  
+
+//  window.nuevo = nuevo;*/
+}
+
+
+document.getElementById("salir").onclick= function(){
+    window.location.reload();
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
-const validator = {
-  // ...
-};
+export const validator = {
+  isValid: (creditCardNumber) => {
+    //console.log(creditCardNumber)
 
-export default validator;
+    var retorno = false;
+
+    let sum = 0;
+    for (var i = 0; i < creditCardNumber.length; i++) {
+      var digito = parseInt(creditCardNumber.substr(i, 1));
+      if (i % 2 == 0) {
+        //console.log("pos", 1);
+        //console.log("val", resultadotarjeta[i]);
+        digito *= 2;
+        //console.log("multiplicar", digito);
+        if (digito > 9) {
+          digito = 1 + (digito % 10);
+          //console.log("mayor10", digito);
+        }
+      }
+      sum += digito;
+
+    }
+    if (sum % 10 == 0) {
+      retorno = true;
+    }
+    if (digito === true) {
+      retorno = true;
+    }
+
+    if (retorno===true) {
+      return "Valida"
+    }
+    else {
+      return "Invalida"
+    }
+
+  },
+  maskify: (creditCardNumber) => {
+  
+    var escondido = "*";
+//let numeroescondido = resultadotarjeta;
+var tarjetaEnmascarada = creditCardNumber.slice(-4);
+if (creditCardNumber.length > 4) {
+  tarjetaEnmascarada = escondido.repeat(creditCardNumber.length - 4) + creditCardNumber.slice(-4)
+}
+
+return tarjetaEnmascarada;
+            
+
+}
+  
+    }
+
